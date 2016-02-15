8f6f8e30a6db428e9b8ee46f0a9966c2658fb06b
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-02-14 23:37:49 -0500

    Remove the localized listing of NHL teams

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,5 @@ module StatsScraper
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    #config.i18n.default_locale = :de
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
