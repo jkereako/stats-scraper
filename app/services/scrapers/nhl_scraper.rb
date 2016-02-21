aa75b214354840f24ecb92c85c4c7d5428cf5d4e
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-02-20 23:58:54 -0500

    Add XPath query for NHL league injuries
    Tested the result and saved the output as `nhl-league-injuries.txt`.

--- app/services/scrapers/nhl_scraper.rb
+++ app/services/scrapers/nhl_scraper.rb
@@ -1,8 +1,8 @@
 # Extracts data from HTML and returns it as a plain text file.
 class NHLScraper
 
-  def initialize(file:)
-    @document = Nokogiri::HTML file
+  def initialize(document:)
+    @document = Nokogiri::HTML document
   end
 
   # Extract the table which holds the league standings for the NHL
@@ -10,6 +10,13 @@ class NHLScraper
     # This XPath works for all league standings: division, conference and
     # wildcard.
     @document.xpath('//div[@id="shsStandings"]/table[2]/tbody/tr').text
+  end
+
+  def league_injuries
+    @document.xpath(
+      '//div[@id="shsNHLInjuries"]/table[contains(concat(" ", normalize-space('\
+      '@class), " "), " shsTable ")] | //div[@id="shsNHLInjuries"]/h2'
+    ).text
   end
 
   def team_roster
--- test/services/scrapers/nhl_scraper_test.rb
+++ test/services/scrapers/nhl_scraper_test.rb
@@ -5,17 +5,36 @@ class NHLScraperTest < ActiveSupport::TestCase
   FIXTURE_PATH = File.join ActiveSupport::TestCase.fixture_path, 'scrapers'
 
   test 'league standings' do
-    file = File.read File.join(FIXTURE_PATH, 'nhl-league-standings.html')
-    scraper = NHLScraper.new file: file
+    file = read_file 'nhl-league-standings.html'
+    scraper = NHLScraper.new document: file
     result = scraper.league_standings
     assert_not_empty result
   end
 
+  test 'league injuries' do
+    file = read_file 'nhl-league-injuries.html'
+    scraper = NHLScraper.new document: file
+    result = scraper.league_injuries
+    assert_not_empty result
+  end
+
   test 'roster' do
-    file = File.read File.join(FIXTURE_PATH, 'nhl-roster.html')
-    scraper = NHLScraper.new file: file
+    file = read_file 'nhl-roster.html'
+    scraper = NHLScraper.new document: file
     result = scraper.team_roster
     assert_not_empty result
+  end
+
+  private
+
+  def read_file(file)
+    File.read File.join FIXTURE_PATH, file
+  end
+
+  # This is a helper method which is only used, for now, in writing the scraped
+  # text to the test/fixtures directory
+  def write_file(file_name:, text:)
+    File.open(file_name, 'w') { |file| file.write text }
   end
 end
 
