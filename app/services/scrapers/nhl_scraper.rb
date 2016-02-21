6816a907cb477e942c3ff0a8ef5145040c707b11
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-02-21 10:54:36 -0500

    Add comments explaining the XPath queries

--- app/services/scrapers/nhl_scraper.rb
+++ app/services/scrapers/nhl_scraper.rb
@@ -8,12 +8,13 @@ class NHLScraper
 
   # Extract the table which holds the league standings for the NHL
   def league_standings
-    # This XPath works for all league standings: division, conference and
-    # wildcard.
+    # Select the table rows `<tr>` within `<div id="shsStandings">`
     @document.xpath('//div[@id="shsStandings"]/table[2]/tbody/tr').text
   end
 
   def league_injuries
+    # The XPath query is confusing because XPath does not support CSS class
+    # selection. Hence, we must use magic.
     @document.xpath(
       '//div[@id="shsNHLInjuries"]/table[contains(concat(" ", normalize-space('\
       '@class), " "), " shsTable ")] | //div[@id="shsNHLInjuries"]/h2'
@@ -21,6 +22,7 @@ class NHLScraper
   end
 
   def team_roster
+    # Select the table `<table>` within `<div id="shsNHLTeamStats">`
     @document.xpath('//div[@id="shsNHLTeamStats"]/table[1]').text
   end
 end
